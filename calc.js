eb544bf55b126417cf42f9b31441cf2620f8409a
LIJI THOMAS <neethuthadathil90@gmail.com>
2023-07-17 16:10:59 +0530

    initial version

---
+++ calc.js
@@ -0,0 +1,16 @@
+function displayData(n)
+{
+    result.value=result.value+n
+}
+function evaluateResult() 
+{
+    result.value=eval(result.value)
+}
+function emptyStr() 
+{
+    result.value=" "
+}
+function backSpace() 
+{
+result.value=result.value.slice(0,-1)
+}
